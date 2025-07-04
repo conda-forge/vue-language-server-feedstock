a0ebdc8aed3dcd1d9248ee84fe3fe4e847b4db87
Daniel Nachun <daniel.nachun@gmail.com>
2025-07-04 11:55:38 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,14 +5,25 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
+
+# remove unneeded symlink
+rm ${PREFIX}/lib/node_modules/@emmetio/css-parser
 
 # Create license report for dependencies
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/vue-language-server << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/@vue/language-server/bin/vue-language-server.js "\$@"
+EOF
+chmod +x ${PREFIX}/bin/vue-language-server
+
 tee ${PREFIX}/bin/vue-language-server.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\vue-language-server %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\@vue\language-server\bin\vue-language-server.js %*
 EOF
